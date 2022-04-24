dbe81e0276c0146c6567e4c76841b6c613f9afce
Elena <lekolupaeva1201@gmail.com>
2022-04-24 18:41:08 +0200

    add binary-search.js

---
+++ src/binary-search.js
@@ -0,0 +1,18 @@
+const binarySearch = (array, item) => {
+  let low = 0;
+  let high = array.length - 1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    let guess = array[mid];
+    if (item === guess) {
+      return mid;
+    }
+    if (guess > item) {
+      high = mid - 1;
+    }
+    if (guess < item) {
+      low = mid + 1;
+    }
+  }
+  return null
+}
